2b3f322f39dd2c53b3d3f082d3a79cbd0c257724
dron3flyv3r <kasperhorn2003@gmail.com>
2021-12-03 09:11:00 +0100

    template

---
+++ Drug Invaders/src/tmplate.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class tmplate {
+
+    //her sætter du din date skal skal bruges gemmen spillet 
+    static int x;
+
+    tmplate(){
+        //burge til at reset spillet her sætter du din default date, som skal kunne ændres
+        x=2;
+    }
+
+    public static void keyReleased(KeyEvent e) {
+       //hvis i skal bruge knapper bruge denne
+    }
+
+    //ellers skriv alle de nødvendie code del i metheds 
+    public void name() {
+        //code
+    }
+
+    //denne skal i havde hvis i skal tegne noget i spillet
+    public void draw(Graphics g) {
+        
+    }
+    
+}
